e88519ad3eff96372a7297cd70ea9e31e7f63e91
thuy-vimaru <90452603+thuy-vimaru@users.noreply.github.com>
2025-06-11 15:13:45 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+ https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txtAdd commentMore actions
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
+  https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+  https://adaway.org/hosts.txt
+  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 node download_lists.js blocklist
